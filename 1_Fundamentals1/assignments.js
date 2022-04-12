f8ecd463bdb5b3e45a1676c4597c22e737bdd23a
abhinav69217 <abhinav0045.be21@chitkara.edu.in>
2022-04-12 21:57:00 +0530

    first commit

---
+++ 1_Fundamentals1/assignments.js
@@ -0,0 +1,7 @@
+let country = "INDIA";
+let continent = "ASIA";
+let population = "BILLIONS";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 1_Fundamentals1/script.js
@@ -0,0 +1,25 @@
+let js= "amazing";
+console.log(40+8+23-10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName="Jonas";
+console.log(firstName);
+
+let $function =27;
+
+let PI =3.1415;
+
+let jsisfun = true;
+console.log(jsisfun);
+
+//template literals
+
+const firstName1 ='aBHINAV';
+const job = 'teacer';
+const birthYear ='1991'
+const year ='2037';
+
+const jonasNew = `I'm ${firstName1}, a ${year-birthYear} years old ${job}    `
+console.log(jonasNew);
